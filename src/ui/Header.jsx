d0320f7a2c5d5b95614ffe8832de81c8e11ab5f0
Sunil <sunilaravindp@gmail.com>
2024-12-15 05:52:21 +0530

    icon height fix

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className=" flex w-full items-center justify-center gap-2 bg-chessbg-800 text-zinc-50 p-3 border-b-2 border-black/30">
       <img
-        className="w-4 sm:w-6 h-fit object-contain"
+        className="w-4 sm:w-6 h-10 object-contain"
         src="https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/PedroPinhata/phpkXK09k.png"
         alt="chess logo"
       />
